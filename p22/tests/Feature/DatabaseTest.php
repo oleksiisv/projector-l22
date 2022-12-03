5a8b639b42fbe87318416ae23bfdc4185f512431
oleksii.svystunov oleksii.svystunov@vaimo.com <oleksii.svystunov@vaimo.com>
2022-12-03 22:50:02 +0200

    Update tests

---
+++ p22/tests/Feature/DatabaseTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseTest extends \Tests\TestCase
+{
+    public function test_create()
+    {
+        $result = DB::connection()->getDatabaseName();
+
+        $this->assertEquals($result, 'default');
+    }
+}
